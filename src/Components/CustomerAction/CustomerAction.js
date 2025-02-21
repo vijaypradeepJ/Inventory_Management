17c750bb1bd84c978f0bdc801fa3b546fb4c6d9c
Vijay pradeep <vijaypradeep3568@gmail.com>
2025-02-21 13:56:21 +0530

    Add search and AddCustomer button

--- src/Components/CustomerAction/CustomerAction.js
+++ src/Components/CustomerAction/CustomerAction.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import TextField from "@mui/material/TextField"
+import Button from "@mui/material/Button"
+import "./CustomerAction.css"
 
-function CustomerAction() {
+function CustomerAction({setisModalOpen,isModalOpen}) {
+  const handleAddCustomer=()=>{
+    setisModalOpen(true);
+  }
   return (
-    <div>CustomerAction</div>
+    <div>
+      <div className='Search'>
+        <TextField id="outlined-search" label="Search field" type="search" size='medium' color='#574964' />        
+        <Button variant='contained' size='small' onClick={handleAddCustomer} >Add Item</Button>
+        </div>
+
+        <div>
+
+        </div>
+        </div>
+   
+    
   )
 }
 
