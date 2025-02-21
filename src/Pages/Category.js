f008ad2f32573e73b588c2b82807caca1445e21e
Vijay pradeep <vijaypradeep3568@gmail.com>
2025-02-21 12:01:05 +0530

    align category main component content

--- src/Pages/Category.js
+++ src/Pages/Category.js
@@ -2,10 +2,6 @@ import React ,{useState} from 'react'
 import ItemAction from '../Components/ItemActions/ItemAction'
 import DataTable from '../Components/Table/DataTable'
 import ItemModal from '../Components/ItemModal/ItemModal';
-
-
-
-
 function Category() {
   const[categoryData,setCategoryData]=useState([]);
   const[isModalOpen,setIsModalOpen]=useState(false);
@@ -14,16 +10,9 @@ function Category() {
       <ItemAction isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen}/>
       <DataTable categoryData={categoryData} setCategoryData={setCategoryData}/>
       {
-        
           isModalOpen &&
           <ItemModal setIsModalOpen={setIsModalOpen} categoryData={categoryData} setCategoryData={setCategoryData} />
-
-     
-        
-      
-      }
-      
-      
+      }  
     </div>
   )
 }
